c78fb997f2f5db27cbdc7563e0f987e5b4c7564d
Souvik Pandit <102818908+Souvik2022@users.noreply.github.com>
2025-07-02 22:38:31 +0530

    fixed the vercel unused varibles

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Pricing from "@/components/Pricing";
 import FAQ from "@/components/FAQ";
-import CTA from "@/components/CTA";
 import Footer from "@/components/Footer";
 import FeaturesListicle from "@/components/FeaturesListicle";
 import Testimonials3 from "@/components/Testimonials3";
